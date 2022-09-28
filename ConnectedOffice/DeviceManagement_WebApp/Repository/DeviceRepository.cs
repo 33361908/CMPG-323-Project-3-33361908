914e7b13590f4d430c610c1d454b68ebc2913be4
QuentinPotgieter <quentinpotgieter@hotmail.co.za>
2022-09-28 10:46:36 +0200

    Final Repository changes, dynamic injection added

--- ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -1,5 +1,11 @@
 ﻿using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace DeviceManagement_WebApp.Repository
 {
@@ -7,6 +13,22 @@ namespace DeviceManagement_WebApp.Repository
     {
         public DeviceRepository(ConnectedOfficeContext context) : base(context)
         {
+
+        }
+
+        // Include Category and Zone
+        public override async Task<IEnumerable<Device>> GetAll()
+        {
+            return await _context.Set<Device>().Include(d => d.Category).Include(d => d.Zone).ToListAsync();
+        }
+        public override async Task<Device> FindById(Guid? id)
+        {
+            if (id == null) return null;
+
+            return await _context.Device
+                .Include(d => d.Category)
+                .Include(d => d.Zone)
+                .FirstOrDefaultAsync(m => m.DeviceId == id);
         }
     }
 }
